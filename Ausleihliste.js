7b3b84a51857b50b143a896a5c60042fd1140afa
Mitch164 <michi.dobler@yahoo.de>
2023-05-08 16:03:39 +0200

    aufräumen

--- Ausleihliste.js
+++ Ausleihliste.js
@@ -2,11 +2,11 @@
  * Extrahiert die Werte für die Anzahlen aus dem Feld für private Ausleiher.
  */
 function EXTRACTANZAHL(input, pattern, groupId) {
-    var match,
-    extractedNumbers = [];
+    var match = [];
+    var extractedNumbers = [];
     if (input) {
-        var rx = new RegExp(pattern, 'g');
-        while (match = rx.exec(input)) {
+        var regex = new RegExp(pattern, 'g');
+        while (match = regex.exec(input)) {
             extractedNumbers.push(match[groupId]);
         }
     }
